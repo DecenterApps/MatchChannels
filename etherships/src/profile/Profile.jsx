c85528e845f8bfcc4c188393a9d6adab4e2e7106
Nikola Klipa <klipa.nikola@gmail.com>
2018-06-13 17:08:47 +0200

    Profile page design implemented

--- etherships/src/profile/Profile.jsx
+++ etherships/src/profile/Profile.jsx
@@ -2,12 +2,78 @@ import React, { Component } from 'react';
 
 import './Profile.css';
 
+import { getUser } from '../services/ethereumService';
+
 class Profile extends Component {
+
+	constructor(props) {
+        super(props);
+
+        this.state = {
+            user: []
+        };
+    }
+
+    async componentDidMount() {
+        const user = await getUser(window.account);
+
+        console.log(user);
+
+        this.setState({
+            user
+        });
+    }
 
     render() {
         return (
-            <div>
-                Profile
+        	<div className='container'>
+	            <div>
+	                <div className='title-front'>
+	                    battleship
+	                </div>
+	                <div className='small-titles'>
+	                	profile
+	                </div>
+	                <div className='big-label'>
+	                {
+						this.state.user.length > 0 && this.state.user[0].toString()
+					}username
+	                </div>
+	                <div className='small-titles'>
+	                	balance
+	                </div>
+	                <div className='big-label'> 
+						{
+							this.state.user.length > 0 && this.state.user[1].toString()
+						} (ETH)
+					</div>
+					<div className="div-flex">
+		                <div className="wrapper">
+		                	<div id="inner1">
+		                		<input className="input" type="text"/> 
+		                	</div>
+		                	<div id="inner2">
+		                		<button className='btn'>Fund</button> 
+		                	</div>
+		                </div>
+
+		                <div className="wrapper">
+		                	<div id="inner1">
+		                		<input className="input" type="text"/> 
+		                	</div>
+		                	<div id="inner2">
+		                		<button className='btn'>Withdraw</button> 
+		                	</div>
+		                </div>
+		            </div>
+					
+					<div className="small-titles">
+						games
+					</div>
+					<div className="big-label">
+						{this.state.user.length > 0 && this.state.user[2].toString()}/{this.state.user.length > 0 && this.state.user[3].toString()}
+					</div>
+	            </div>
             </div>
         );
     }
